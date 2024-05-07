cc5364b9915045bc3af57d0621b265e591331509
Tathagat Saha <99318598+tatz210588@users.noreply.github.com>
2024-05-07 21:27:11 +0200

    com

--- App.tsx
+++ App.tsx
@@ -1,11 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Pressable, View } from "react-native";
+import { useEffect } from "react";
+
+import {
+  WalletConnectModal,
+  useWalletConnectModal,
+} from "@walletconnect/modal-react-native";
+
+const projectID = "2eabed2abc1d75170d0d140475632930";
+
+const providerMetadata = {
+  name: "GrowLimitLess",
+  description: "A Unified Crypto Interface",
+  url: "https://launch.growlimitless.app",
+  icons: ["https://your-project-logo.com"],
+  redirect: {
+    native: "YOUR_APP_SCHEME://",
+    universal: "YOUR_APP_UNIVERSAL_LINK.com",
+  },
+};
 
 export default function App() {
+  const { isOpen, open, close, provider, isConnected, address } =
+    useWalletConnectModal();
+  
+  const handleButtonPress = async () => {
+    if (isConnected) {
+      return provider?.disconnect();
+    }
+
+    return open();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Web3 WalletConnection Testing</Text>
+      <Text>{isConnected ? address : "No Wallet Available"}</Text>
+      <Pressable onPress={() => handleButtonPress()} style={{ marginTop: 56 }}>
+        <Text>{isConnected ? "Disconnect" : "Connect"}</Text>
+      </Pressable>
+
+      <WalletConnectModal
+        projectId={projectID}
+        providerMetadata={providerMetadata}
+      />
     </View>
   );
 }
@@ -13,8 +50,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
